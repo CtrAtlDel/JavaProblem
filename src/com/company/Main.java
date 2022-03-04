94f35183492aec3630984f91aec4a00a0d67f7e0
CtrAtlDelete <kvasovski34@gmail.com>
2022-03-04 19:11:54 +0300

    Add window .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Begin: ");
-
+        System.out.println("Begin ");
+        JFrame window = new JFrame("Tic tac toe");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(400,400);
+        window.setLayout(new BorderLayout());
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        System.out.println("End ");
     }
 }
