fd68076532c263f49d6fd511583877564c674447
CtrAtlDelete <kvasovski34@gmail.com>
2022-03-07 19:04:30 +0300

    Add const

---
+++ Lab2/src/main/java/Crypto/Const/Const.java
@@ -0,0 +1,6 @@
+package Crypto.Const;
+
+public class Const {
+    public static final int MinSha = 15;
+    public static final int MaxSha = 20;
+}
--- Lab2/src/main/java/Crypto/Sha/ShaXX.java
+++ Lab2/src/main/java/Crypto/Sha/ShaXX.java
@@ -1,21 +1,20 @@
 package Crypto.Sha;
-import org.jetbrains.annotations.NotNull;
+
+import Crypto.Const.Const;
 
 import java.util.Arrays;
 
 public final class ShaXX extends Sha256 {
     public static byte[] getShaByString(String original, int size) throws Exception {
-        if (size < 15 || size > 20)
-            throw new Exception("Size < 0 or Size > 20");
+        if (size < Const.MinSha || size > Const.MaxSha) throw new Exception("Size < 0 or Size > 20");
 
         var result = getShaByString(original);
 
         return Arrays.copyOfRange(result, 0, size);
     }
 
-    public static @NotNull String getShaByByte(String original, int size) throws Exception {
-        if (size < 15 || size > 20)
-            throw new Exception("Size < 0 or Size > 20");
+    public static String getShaByByte(String original, int size) throws Exception {
+        if (size < Const.MinSha || size > Const.MaxSha) throw new Exception("Size < 0 or Size > 20");
 
         return byteToString(getShaByString(original, size));
     }
