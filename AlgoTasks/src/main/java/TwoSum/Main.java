3675f1eb54abc359cf8e6d130f8e62d07269354c
CtrAtlDelete <kvasovski34@gmail.com>
2022-05-03 23:53:22 +0300

    Add not optmized solution of twoSum

---
+++ AlgoTasks/src/main/java/TwoSum/Main.java
@@ -0,0 +1,28 @@
+package TwoSum;
+
+import java.util.Arrays;
+
+public class Main {
+
+
+    // как сделать сложность не равную О(n^2)?
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && (i != j)) {
+                    int[] newArr = new int[2];
+                    newArr[0] = i;
+                    newArr[1] = j;
+                    return newArr;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 2, 4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(array, target)));
+    }
+}
