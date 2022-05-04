c2d51d093ee29e24803c0b1bd492f9157312a8e5
CtrAtlDelete <kvasovski34@gmail.com>
2022-05-04 12:31:33 +0300

    Some change

--- AlgoTasks/src/main/java/TwoSum/Main.java
+++ AlgoTasks/src/main/java/TwoSum/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
 
-    // как сделать сложность не равную О(n^2)?
+    // как сделать сложность не равную О(n^2)? C помощью двух указателей
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
