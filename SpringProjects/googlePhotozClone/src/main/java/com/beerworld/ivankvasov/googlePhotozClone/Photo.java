84a146c52c57086c0ba9233cf968a8a511574dc8
CtrAtlDelete <kvasovski34@gmail.com>
2022-05-04 13:39:32 +0300

    Change db, using HashMap

---
+++ SpringProjects/googlePhotozClone/src/main/java/com/beerworld/ivankvasov/googlePhotozClone/Photo.java
@@ -0,0 +1,33 @@
+package com.beerworld.ivankvasov.googlePhotozClone;
+
+
+public class Photo {
+    private String id;
+
+    private String fileName;
+
+    public Photo() {}
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+
+}
--- SpringProjects/googlePhotozClone/src/main/java/com/beerworld/ivankvasov/googlePhotozClone/PhotozController.java
+++ SpringProjects/googlePhotozClone/src/main/java/com/beerworld/ivankvasov/googlePhotozClone/PhotozController.java
@@ -1,13 +1,32 @@
 package com.beerworld.ivankvasov.googlePhotozClone;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PhotozController {
+    private Map<String, Photo> db = new HashMap<>(){{
+       put("1", new Photo("1", "hello.jpg"));
+    }};
 
     @GetMapping("/")
     public String hello() {
         return "Hello world";
+    }
+
+    @GetMapping("/photoz") // return all photo
+    public Collection<Photo> get() {
+        return  db.values();
+    }
+
+    @GetMapping("/photoz/{id}") // retrun photo by id
+    public Photo get(@PathVariable String id) {
+        return db.get(id);
     }
 }
