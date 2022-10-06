4a3e494e1ce7f3326672656ddaac47f7f3e64765
CtrAtlDelete <kvasovski34@gmail.com>
2022-10-06 23:23:12 +0300

    Add lazy stream example

--- CLI/easyDocker/src/main/java/Program.java
+++ CLI/easyDocker/src/main/java/Program.java
@@ -2,8 +2,10 @@ import learningLambda.Converter;
 import learningLambda.Dog;
 import learningLambda.Racoon;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) {
@@ -11,6 +13,23 @@ public class Program {
         var dog = new Dog("Bob", 5, 3);
         Converter<Dog, Racoon> converter = x -> new Racoon(dog.getName(), dog.getAge(), dog.getWeight());
         var racoon = converter.convert(dog);
-        System.out.println(racoon);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Bob");
+        arrayList.add("Robert");
+        arrayList.add("Adam");
+        arrayList.add("David");
+        arrayList.add("Valentin");
+
+        int sizeOfNewArrayList = 5;
+
+
+        // Lazy stream generator
+         Stream.generate(() -> {
+                    int value = (int) (Math.random() * arrayList.size());
+                    return arrayList.get(value);
+                }).limit(sizeOfNewArrayList)
+                .forEach(System.out::println);
+
     }
 }
