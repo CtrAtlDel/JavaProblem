192055837772af674922d172956aa7c4bbd15a00
CtrAtlDelete <kvasovski34@gmail.com>
2022-11-15 01:20:53 +0300

    Rename src folder

--- kafka-example/src/main/java/com/example/kafkaMissMe/KafkaExampleApplication.java
+++ kafka-example/src/main/java/com/example/kafkaMissMe/KafkaExampleApplication.java
@@ -1,4 +1,4 @@
-package com.example.kafkaexample;
+package com.example.kafkaMissMe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- kafka-example/src/test/java/com/example/kafkaMissMe/KafkaExampleApplicationTests.java
+++ kafka-example/src/test/java/com/example/kafkaMissMe/KafkaExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.kafkaexample;
+package com.example.kafkaMissMe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
