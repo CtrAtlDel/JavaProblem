abd26f505844d6c4aed1bd91e38cf5a6d36b184e
CtrAtlDelete <kvasovski34@gmail.com>
2022-11-16 03:09:31 +0300

    Init message controller

---
+++ kafka-example/src/main/java/com/example/kafkaMissMe/MessageContoller/MessageController.java
@@ -0,0 +1,9 @@
+package com.example.kafkaMissMe.MessageContoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+public class MessageController {
+}
